54ab631687e4f5efe03bf8e6c633cd98865c67fa
Marius <marius.lledouarin@hotmail.fr>
2021-11-19 12:52:16 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById('btn')
+const nav = document.getElementById('nav')
+const btn2D = document.getElementById('2D')
+const btn3D = document.getElementById('3D')
+const container = document.getElementById('container')
+const darkMode = document.getElementById('toggle')
+
+btn2D.addEventListener("click", () => {
+    createNotification("2D")
+});
+
+btn3D.addEventListener("click", () => {
+    createNotification("3D")
+});
+
+toggle.addEventListener("change", (event)=> {
+    document.body.classList.toggle('dark', event.target.checked);
+})
+
+function createNotification(dimension) {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+
+    container.appendChild(notif);
+
+    notif.innerText = "Vous êtes passé en " + dimension;
+
+    setTimeout(() => {
+        notif.remove();
+    }, 3000);
+}
+
+btn.addEventListener("click", () => {
+    nav.classList.toggle('active');
+    btn.classList.toggle('active');
+});
