3bb6ca75df40339617fb5866addc293c01995bd4
Marius Le Douarin <marius.lledouarin@hotmail.fr>
2022-02-04 08:39:40 +0100

    Nettoyage du fichier HTML

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ btn3D.addEventListener("click", () => {
     createNotification("3D")
 });
 
-toggle.addEventListener("change", (event)=> {
+toggle.addEventListener("change", (event) => {
     document.body.classList.toggle('dark', event.target.checked);
 })
 
@@ -36,7 +36,85 @@ btn.addEventListener("click", () => {
 });
 
 function removeAllMarkers(list) {
-    for (var i = currentMarkers.length-1;i>=0;i--) {
+    for (var i = currentMarkers.length - 1; i >= 0; i--) {
         list[i].remove()
     }
+}
+
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-1.6364151881843991, 48.118105356676736]
+            },
+            properties: {
+                title: 'DON DU SANG',
+                description: 'ESIR'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-1.635005, 48.118632]
+            },
+            properties: {
+                title: 'DON DU SANG',
+                description: 'RU'
+            }
+        }
+    ]
+};
+
+map.on('click', (e) => {
+    document.getElementById('info').innerHTML =
+        //  Coordonées x y de la position de la souris lors de l'event click de souris
+        JSON.stringify(e.point) +
+        '<br />' +
+        // Position en terme de latitude et longitude de l'event
+        JSON.stringify(e.lngLat.wrap());
+
+    // Ajout de la donnée d'un lieu dans le geojson
+    feature = {}
+    feature['type'] = 'Feature'
+    feature['geometry'] = {
+        'type': 'Point',
+        'coordinates': e.lngLat,
+    }
+    feature['properties'] = { 'title': 'osef', 'description': 'UN LIEU INCROYABLE' }
+    geojson['features'].push(feature)
+    sourceRefresh();
+    console.log(geojson['features'])
+});
+
+var currentMarkers = [];
+function sourceRefresh() {
+    currentMarkers
+    // On ajoute le marker à la map
+    for (const feature of geojson.features) {
+        // On crée l'élement html pour toutes les features
+        const el = document.createElement('div');
+        el.className = 'marker';
+        // et on les ajoute à la map
+        var m = new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .setPopup(
+                // Ajout du popup
+                new mapboxgl.Popup({ offset: 25 })
+                    .setHTML(
+                        `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                    )
+            )
+            .addTo(map);
+        currentMarkers.push(m)
+    }
+}
+
+function empty() {
+    removeAllMarkers(currentMarkers)
+    currentMarkers = []
+    geojson.features = []
 }
